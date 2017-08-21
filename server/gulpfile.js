b2c443b71b58fca08517e1e49e22dc6e2ceaa9ed
Liutong Chen <liutong.clt@gmail.com>
2017-08-20 19:02:00 -0700

    Add nodejs server-side code.

---
+++ server/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+
+gulp.task('sayhaha', () => {
+    console.log("haha");
+});
+
+gulp.task('run', () => {
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            port: 3000,
+        },
+        ignore: ['./node_modules/**']
+    }).on('restart', () => {
+        console.log("Restarting..")
+    });
+});
